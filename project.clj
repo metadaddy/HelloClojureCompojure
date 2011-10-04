88de3aed617e988ef4dcede60f701cc268e193d5
Pat Patterson <ppatterson@salesforce.com>
2011-10-04 15:44:54 -0700

    Fixed an embarrasing bug

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject clojure-force-sample "1.0.0"
   :description "Force.com REST API Sample in Clojure"
   :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [compojure "0.4.1"]
                  [ring/ring-jetty-adapter "0.2.5"]]
   :dev-dependencies [[lein-run "1.0.0"]
